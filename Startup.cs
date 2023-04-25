aae1765c60a89b548ef716ce9d06f0746576ed1e
Dev <dev@epmoworx.com>
2023-04-25 15:38:59 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+[assembly: OwinStartup(typeof(API.Startup))]
+namespace API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "http://localhost:50373", //some string, normally web url,
+                        ValidAudience = "http://localhost:50373",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my_secret_key_12345"))
+                    }
+                });
+        }
+    }
+}
